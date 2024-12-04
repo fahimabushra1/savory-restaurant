96f48b46daf8c50ff101bd0481e542ff6efeede0
fatima arifeen <fatima.arifeen24@gmail.com>
2024-12-04 20:35:08 +0600

    jwt token

--- src/pages/dashboard/AllUsers.jsx
+++ src/pages/dashboard/AllUsers.jsx
@@ -3,16 +3,61 @@ import { useQuery } from "@tanstack/react-query";
 import HelmetTitle from "../../components/shared/HelmetTitle";
 import SectionTitle from "../../components/shared/SectionTitle";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const {data: users = []} = useQuery({
+    const {data: users = [], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleMakeAdmin = user =>{
+axiosSecure.patch(`/users/admin/${user._id}`)
+.then(res =>{
+    console.log(res.data)
+    if(res.data.modifiedCount > 0){
+        refetch();
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is admin now!`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+    }
+})
+    }
+
+    const handleDeleteUser = user =>{
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            axiosSecure.delete(`/users/${user._id}`)
+            .then(res=>{
+              if(res.data.deletedCount > 0){
+                refetch();
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success"
+                });
+              }
+            })
+          }
+        });
+      }
     return (
         <div className="-mt-8">
         <HelmetTitle title={"cart"}/>
@@ -58,12 +103,16 @@ const AllUsers = () => {
      {user.name}
      </td>
      <td>{user.email}</td>
+     <th className=" text-orange-600">
+       {
+        user.role === 'admin' ? 'ADMIN' :
+        <button onClick={()=>{handleMakeAdmin(user)}} className="btn btn-outline"><FaUsers className="bg-orange-600 p-0 rounded-md text-white text-2xl"/></button>
+       }
+    </th>
      <th>
-        <button>delete</button>
-       {/* <button onClick={()=>{handleDelete(item._id)}} className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-600"/></button> */}
+       <button onClick={()=>{handleDeleteUser(user)}} className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-600"/></button>
      </th>
    </tr>)}
-  
  </tbody>
 </table>
 </div>
